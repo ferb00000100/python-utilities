7ec52b19de7f5bc07eac8f94222c0960314efc86
jason martin <jason.martin@infor.com>
2023-09-14 14:22:01 -0600

    updated counter-sorter

--- log_parsers/counter_sorter.py
+++ log_parsers/counter_sorter.py
@@ -12,10 +12,12 @@ def count_sort():
 	with open('ports.csv') as data_file:
 		data = csv.reader(data_file)
 		for port in data:
+			print(port[3])
 			port_list.append(port[3])
 		port_list.sort()
 		for x in port_list:
 			data = port_list.count(x), x
+			print(data)
 			final_list.append(data)
 		final_list = list(set(final_list))
 		for line in final_list:
